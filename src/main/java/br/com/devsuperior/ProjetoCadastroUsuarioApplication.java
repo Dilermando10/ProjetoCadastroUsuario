7019cf894655e1ba340e7f9e2cc9caedcde0cccb
Dilermando <dilermando10@gmail.com>
2022-01-17 10:27:10 -0300

    Projeto Cadastro de Usuário

---
+++ src/main/java/br/com/devsuperior/ProjetoCadastroUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCadastroUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCadastroUsuarioApplication.class, args);
+	}
+
+}
